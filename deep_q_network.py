39648717a040723cd00e0ba3524d6fcf93210fa6
zgjsxx <119160524@qq.com>
2026-01-26 22:38:22 +0800

    revise code to run

--- deep_q_network.py
+++ deep_q_network.py
@@ -109,7 +109,7 @@ def trainNetwork(s, readout, h_fc1, sess):
     checkpoint = tf.train.get_checkpoint_state("saved_networks")
     if checkpoint and checkpoint.model_checkpoint_path:
         saver.restore(sess, checkpoint.model_checkpoint_path)
-        print("Successfully loaded:", checkpoint.model_checkpoint_path_)
+        print("Successfully loaded:", checkpoint.model_checkpoint_path)
     else:
         print("Could not find old network weights")
 
